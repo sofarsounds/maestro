089bf40de15146fc94742cd79aca7e6f68201e2a
David Floegel <mail@davidfloegel.com>
2019-06-13 10:10:10 +0100

    merge master in and fix conflicts

--- packages/uikit/src/index.tsx
+++ packages/uikit/src/index.tsx
@@ -1,9 +1,5 @@
-<<<<<<< HEAD
 // TODO rename logo & typography to Logo & Typography
 
 export { default as Logo } from './atoms/logo';
 export * from './atoms/Button';
 export * from './atoms/typography';
-=======
-//export { default as Button } from './button';
->>>>>>> master
