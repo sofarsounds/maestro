fae42a7fbe71b4c1d75c9f036558a198fdc1076f
David Floegel <mail@davidfloegel.com>
2019-06-20 14:21:58 +0100

    rename mediaFileMock.ts to mediaFileMock.js to avoid build failures

--- packages/uikit/jest.config.js
+++ packages/uikit/jest.config.js
@@ -2,7 +2,7 @@ module.exports = {
   testRegex: '/.*\\.test\\.(tsx|ts)$',
   transform: {
     '^.+\\.(tsx|ts)?$': 'ts-jest',
-    '^.+\\.(svg)?$': './src/test/mediaFileMock.ts'
+    '^.+\\.(svg)?$': './src/test/mediaFileMock.js'
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'svg'],
   snapshotSerializers: ['enzyme-to-json/serializer'],
