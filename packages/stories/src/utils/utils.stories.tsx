7a8b801693d6dfdeab7ef2ee57d1ced5eef07bcf
David Floegel <mail@davidfloegel.com>
2019-06-24 11:51:11 +0100

    add new withShadow util that can be reused on other elements

---
+++ packages/stories/src/utils/utils.stories.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import styled from 'styled-components';
+// import { action } from '@storybook/addon-actions';
+// import { withKnobs, text, number, boolean } from '@storybook/addon-knobs';
+
+import { Grid, Col, withShadow } from 'uikit';
+
+const Block = styled<any>(Col)`
+  height: 100px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  ${withShadow};
+`;
+
+storiesOf('04 / Utils|withShadow', module).add('withShadow', () => (
+  <Grid gap="50px">
+    {[1, 2, 3, 4, 5].map(d => (
+      <Block sm={4} md={2} depth={d}>
+        depth = {d}
+      </Block>
+    ))}
+  </Grid>
+));
---
+++ packages/uikit/src/util/withShadow.ts
@@ -0,0 +1,34 @@
+import { css } from '../lib/styledComponents';
+
+interface ShadowProps {
+  depth: 1 | 2 | 3 | 4 | 5;
+}
+
+export default ({ depth }: ShadowProps) => css`
+  ${depth <= 1 &&
+    css`
+      box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.1);
+    `}
+
+  ${depth === 2 &&
+    css`
+      box-shadow: 0 3px 6px 0 rgba(0, 0, 0, 0.16),
+        0 3px 6px 0 rgba(0, 0, 0, 0.23);
+    `}
+
+  ${depth === 3 &&
+    css`
+      box-shadow: 0 10px 20px 0 rgba(0, 0, 0, 0.19);
+    `}
+
+  ${depth === 4 &&
+    css`
+      box-shadow: 0 14px 28px 0 rgba(0, 0, 0, 0.25);
+    `}
+
+  ${depth >= 5 &&
+    css`
+      box-shadow: 0 19px 38px 0 rgba(0, 0, 0, 0.3),
+        0 15px 12px 0 rgba(0, 0, 0, 0.22);
+    `}
+`;
