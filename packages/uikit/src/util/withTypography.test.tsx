db6eeab6356263ed5f068099195681e6d9fccf09
Daniel Williams <info@thinkdan.co.uk>
2019-07-01 11:28:48 +0100

    Add withTypography to storybook and add assosiated tests

---
+++ packages/uikit/src/util/withTypography.test.tsx
@@ -0,0 +1,65 @@
+import React from 'react';
+import styled from 'styled-components';
+import withTypography from './withTypography';
+import { mount } from 'enzyme';
+
+const Typography = styled.h1<any>`
+  color: yellow;
+  ${withTypography};
+`;
+
+const generateTestCase = (
+  component: any,
+  cssProperty: string,
+  cssVariants: string[] | boolean[] | number[],
+  propVariant: string
+) => {
+  cssVariants.forEach((cssValue: any, index: any) => {
+    let dynamicProp = { [propVariant]: cssValue };
+    let wrapper = mount(<Typography {...dynamicProp} />);
+    expect(wrapper).toHaveStyleRule(cssProperty, cssValue);
+  });
+};
+
+describe('Util: withTypography', () => {
+  it('adds an aligned property to element', () => {
+    expect(mount(<Typography align={'center'} />)).toHaveStyleRule(
+      'color',
+      'yellow'
+    );
+
+    expect(mount(<Typography align={'center'} />)).toHaveStyleRule(
+      'text-align',
+      'center'
+    );
+  });
+
+  it('adds align properties to element', () => {
+    generateTestCase(
+      Typography,
+      'text-align',
+      ['center', 'left', 'right', 'none'],
+      'align'
+    );
+  });
+
+  it('adds an underline to the element', () => {
+    expect(mount(<Typography underline />)).toHaveStyleRule(
+      'text-decoration',
+      'underline'
+    );
+  });
+
+  it('adds a line height property to element', () => {
+    generateTestCase(Typography, 'line-height', ['12', '24'], 'lineHeight');
+  });
+
+  it('adds font-weigh property to element', () => {
+    generateTestCase(
+      Typography,
+      'font-weight',
+      ['lighter', 'regular', 'bold'],
+      'fontWeight'
+    );
+  });
+});
---
+++ packages/uikit/src/util/withTypography.ts
@@ -0,0 +1,35 @@
+import { css } from '../lib/styledComponents';
+
+interface TypographyProps {
+  align: 'center' | 'left' | 'right';
+  underline: boolean;
+  lineHeight: number;
+  fontWeight: 'lighter' | 'regular' | 'bold';
+}
+
+export default ({
+  align,
+  underline,
+  lineHeight,
+  fontWeight
+}: TypographyProps) => css`
+  ${!!align &&
+    css`
+      text-align: ${align};
+    `};
+
+  ${!!underline &&
+    css`
+      text-decoration: underline;
+    `};
+
+  ${!!lineHeight &&
+    css`
+      line-height: ${lineHeight};
+    `};
+
+  ${!!fontWeight &&
+    css`
+      font-weight: ${fontWeight};
+    `};
+`;
