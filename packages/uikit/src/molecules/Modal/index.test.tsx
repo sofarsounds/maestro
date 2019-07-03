13d03061e7abf217ae1fb7df2f24865337dc892d
Adam Bull <adamjamesbull@googlemail.com>
2019-07-03 15:46:54 +0100

    Adds basic tests for modal

--- packages/uikit/src/molecules/Modal/index.test.tsx
+++ packages/uikit/src/molecules/Modal/index.test.tsx
@@ -3,6 +3,7 @@ import { ReactWrapper } from 'enzyme';
 import { mountWithTheme } from '../../test';
 
 import { Modal } from './';
+import { LinkButton as Button } from '../../atoms/Button';
 
 let wrapper: ReactWrapper;
 
@@ -22,8 +23,36 @@ describe('<Modal />', () => {
       wrapper = setup(true, hideMock, <>Some children</>);
     });
 
-    it('renders all of constituent elements of the modal', () => {
-      console.log(wrapper);
+    it('matches the snapshot', () => {
+      expect(wrapper).toMatchSnapshot();
+    });
+  });
+
+  describe('Exiting the modal when it is open', () => {
+    beforeEach(() => {
+      wrapper = setup(true, hideMock, <>Some visible text</>);
+    });
+
+    it('closes the modal when you click on the X button', () => {
+      const xButton = wrapper.find(Button);
+
+      xButton.simulate('click');
+
+      expect(hideMock).toHaveBeenCalledTimes(1);
+    });
+  });
+
+  describe('When isShowing is false', () => {
+    beforeEach(() => {
+      wrapper = setup(false, hideMock, <>Some other children</>);
+    });
+
+    it('matches the snapshot', () => {
+      expect(wrapper).toMatchSnapshot();
+    });
+
+    it('does not render anything', () => {
+      expect(wrapper).toEqual({});
     });
   });
 });
