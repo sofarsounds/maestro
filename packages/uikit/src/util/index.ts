fe3313b5988baee0f972f28760d10616ef4264e0
David Floegel <mail@davidfloegel.com>
2019-07-08 16:32:54 +0100

    add interfaces to typography so types file gets generated

--- packages/uikit/src/util/index.ts
+++ packages/uikit/src/util/index.ts
@@ -1,4 +1,7 @@
 export { default as withSpacing } from './withSpacing';
 export { default as withShadow } from './withShadow';
-export { default as withTypography } from './withTypography';
+export {
+  default as withTypography,
+  WithTypographyProps
+} from './withTypography';
 export { default as withFlex, WithFlexProps } from './withFlex';
