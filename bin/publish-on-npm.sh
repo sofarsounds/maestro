4d0ff08f0b17b34df1c3c0abb64775b2c1212f21
David Floegel <mail@davidfloegel.com>
2019-07-12 17:44:41 +0100

    remove lerna, storybook for now, move uikit to highest level and make build work

--- bin/publish-on-npm.sh
+++ bin/publish-on-npm.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 set -e
 
-cd packages/uikit
-
-echo "//registry.npmjs.org/:_authToken=$NPM_TOKEN" > ~/repo/packages/uikit/.npmrc
+echo "//registry.npmjs.org/:_authToken=$NPM_TOKEN" > ~/repo/.npmrc
 
 npm publish --access public
