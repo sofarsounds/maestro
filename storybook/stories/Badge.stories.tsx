0b05911f9802b80c2df1254d2efa40ee55d8ae99
David Floegel <mail@davidfloegel.com>
2019-07-15 13:38:01 +0100

    remove unused import

--- storybook/stories/Badge.stories.tsx
+++ storybook/stories/Badge.stories.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 import { withKnobs, text, number, boolean } from '@storybook/addon-knobs';
-import withInfo from '../helpers/withInfo';
 
 import { Badge } from '../../src';
 
