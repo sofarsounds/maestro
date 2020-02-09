6933bf1f3c3f1b0b7fc876d9f8634bfc2859313c
David Floegel <mail@davidfloegel.com>
2020-02-09 14:14:20 +0000

    make MenuHeader sticky

--- src/atoms/MenuHeader/index.tsx
+++ src/atoms/MenuHeader/index.tsx
@@ -10,6 +10,10 @@ interface Props {
 export default styled(Overline)`
   ${({ theme }) => css`
     padding: ${theme.ruler[1]}px ${theme.ruler[4]}px;
+    background: ${theme.colors.whiteDenim};
+    position: sticky;
+    display: block;
+    top: 0;
 
     color: ${theme.colors.primary};
   `}
