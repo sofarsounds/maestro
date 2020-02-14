484b0694330c57c42ff3c6891349d9e14782b397
David <mail@davidfloegel.com>
2020-02-14 16:21:34 +0000

    remove Popper component

--- src/index.tsx
+++ src/index.tsx
@@ -13,7 +13,6 @@ export { default as Container } from './atoms/Container';
 export { default as Menu } from './atoms/Menu';
 export { default as MenuHeader } from './atoms/MenuHeader';
 export { default as MenuItem } from './atoms/MenuItem';
-export { default as Popper } from './atoms/Popper';
 export { default as Portal } from './atoms/Portal';
 export { default as Grid } from './atoms/Grid';
 export { default as Icon } from './atoms/Icon';
