a353d40780efa80bd900736e90bf6ec0cef34bcc
jsteinb <jason.steinbach@sofarsounds.com>
2020-08-28 18:08:34 -0400

    rm spacing concerns from this branch

--- src/util/withSpacing/index.ts
+++ src/util/withSpacing/index.ts
@@ -1,6 +1,6 @@
 import { css } from '../../lib/styledComponents';
 
-import theme, { breakPoints, responsiveSizes } from '../../theme';
+import theme, { breakPoints } from '../../theme';
 
 export interface WithSpacingProps {
   // global margin & padding
@@ -16,9 +16,6 @@ export interface WithSpacingProps {
   pr?: number | number[];
   pb?: number | number[];
   pl?: number | number[];
-
-  // responsive
-  responsive?: boolean;
 }
 
 export const generateCssProperty = (prop: string, value: string): string => {
@@ -38,12 +35,7 @@ export const generateCssProperty = (prop: string, value: string): string => {
 };
 
 const breakPointKeys = Object.keys(breakPoints);
-export const generateSpace = (
-  screenSize: string,
-  prop: string,
-  size: any,
-  responsive: boolean
-) => {
+export const generateSpace = (screenSize: string, prop: string, size: any) => {
   if (Array.isArray(size)) {
     let styles: any = [];
     const sizeIndex = breakPointKeys.indexOf(screenSize);
@@ -52,22 +44,6 @@ export const generateSpace = (
       if (size[sizeIndex]) {
         styles.push(
           generateCssProperty(prop, `${theme.ruler[Number(size[sizeIndex])]}px`)
-        );
-      }
-    }
-
-    return styles;
-  } else if (responsive) {
-    let styles: any = [];
-    const sizeIndex = breakPointKeys.indexOf(screenSize);
-
-    if (sizeIndex > -1) {
-      if (responsiveSizes[size] && responsiveSizes[size][sizeIndex]) {
-        styles.push(
-          generateCssProperty(
-            prop,
-            `${theme.ruler[Number(responsiveSizes[size][sizeIndex])]}px`
-          )
         );
       }
     }
@@ -83,7 +59,7 @@ const loop = (screenSize: string, props: any) => {
 
   Object.keys(props).forEach((key: string) => {
     if (/^(m|p)(t|b|r|l)?$/.test(key)) {
-      s += generateSpace(screenSize, key, props[key], props.responsive);
+      s += generateSpace(screenSize, key, props[key]);
     }
   });
 
