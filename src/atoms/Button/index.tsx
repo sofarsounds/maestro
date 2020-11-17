a92256526989987f724aeb3e7467edb7651d79f0
Skim Kimball <skim.kimball@sofarsounds.com>
2020-11-17 15:34:24 -0500

    actually fixed the background color I broke

--- src/atoms/Button/index.tsx
+++ src/atoms/Button/index.tsx
@@ -154,7 +154,7 @@ export const OutlineButton = styled(PrimaryButton)<OutlineButtonProps>`
     }
 
     &:disabled {
-      background-color: ${theme.colors.backToBlack};
+      background-color: ${theme.colors.whiteDenim};
       color: ${theme.colors.blueSmoke};
 
       i:before {
