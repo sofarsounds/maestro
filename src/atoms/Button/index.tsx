4551e27593f630b9c9738a8fe9d7b7d9969f547a
jsteinb <jason.steinbach@sofarsounds.com>
2020-11-20 10:38:19 -0500

    correct formatting

--- src/atoms/Button/index.tsx
+++ src/atoms/Button/index.tsx
@@ -29,6 +29,7 @@ export const buttonTextStyle = ({ theme }: any) => css`
 export const BaseButton = styled.button<ButtonProps>`
   ${({ theme, loading, small, block }) => css`
     border-radius: ${theme.borderRadius.button};
+
     cursor: pointer;
     font-size: ${theme.fontSizes.button};
     font-weight: 600;
@@ -38,32 +39,39 @@ export const BaseButton = styled.button<ButtonProps>`
     outline: none;
     text-decoration: none;
     text-transform: uppercase;
+
     &:disabled {
       cursor: not-allowed;
       pointer-events: none;
     }
+
     // icon tag
     i {
       margin-right: ${theme.ruler[2]}px;
     }
+
     ${loading &&
       css`
         pointer-events: none;
         cursor: disabled;
+
         i {
           opacity: 0;
         }
       `}
+
     ${small &&
       css`
         height: ${theme.dimensions.button.small.height};
         padding: ${theme.dimensions.button.small.padding};
       `}
+
     ${block &&
       css`
         display: block;
         width: 100%;
       `}
+
   `}
 `;
 
@@ -72,25 +80,31 @@ export const PrimaryButton = styled(BaseButton)`
     background: ${theme.colors.primary};
     border: 1px solid ${theme.colors.primary};
     color: ${theme.colors.whiteDenim};
+
     &:hover {
       background-color: ${theme.colors.green900};
     }
+
     &:focus {
       background-color: ${theme.colors.green700};
     }
+
     &:disabled {
       background-color: ${theme.colors.blueSmoke};
       border-color: ${theme.colors.blueSmoke};
     }
+
     i:before {
       color: ${theme.colors.whiteDenim};
     }
+
     ${loading &&
       css`
         color: transparent !important;
         background-color: ${theme.colors.blueSmoke};
         border-color: ${theme.colors.blueSmoke};
         position: relative;
+
         :after {
           animation: ${rotate360} 0.5s linear infinite;
           border: 0.1rem solid ${theme.colors.whiteDenim};
@@ -120,66 +134,86 @@ export const OutlineButton = styled(PrimaryButton)<OutlineButtonProps>`
   ${({ theme, loading, color }) => css`
     background: transparent;
     color: ${theme.colors.primary};
+
     i:before {
       color: ${theme.colors.primary};
     }
+
     &:hover {
       border-color: ${theme.colors.green900};
       color: ${theme.colors.green900};
       background-color: transparent;
+
       i:before {
         color: ${theme.colors.green900};
       }
     }
+
     &:focus {
       background-color: ${makeOpaque(theme.colors.green600, 0.1)};
     }
+
     &:disabled {
       background-color: transparent;
       color: ${theme.colors.blueSmoke};
+
       i:before {
         color: ${theme.colors.blueSmoke};
       }
     }
+
     ${color === 'black' &&
       css`
         border-color: ${theme.colors.blackBetty};
         color: ${theme.colors.backToBlack};
+
         i:before {
           color: ${theme.colors.backToBlack};
         }
+
         &:hover {
           border-color: ${theme.colors.backToBlack};
           color: ${theme.colors.backToBlack};
+
           i:before {
             color: ${theme.colors.backToBlack};
           }
         }
+
         &:focus {
           background-color: ${theme.colors.macyGrey};
         }
       `}
+
     ${color === 'white' &&
       css`
         border-color: ${theme.colors.whiteDenim};
         color: ${theme.colors.whiteDenim};
         background-color: transparent;
+
         i:before {
           color: ${theme.colors.whiteDenim};
         }
+
         &:hover {
           border-color: ${theme.colors.whiteDenim};
           color: ${theme.colors.whiteDenim};
           background-color: transparent;
+
           i:before {
             color: ${theme.colors.whiteDenim};
           }
         }
+
         &:focus {
           border-color: ${theme.colors.silverSprings};
           background: rgba(219, 219, 219, 0.1); //macyGrey at 10%
         }
+        &:disabled {
+          background-color: ${theme.colors.backToBlack};
+        }
       `}
+
     ${loading &&
       css`
         :after {
@@ -196,18 +230,23 @@ export const LinkButton = styled(OutlineButton)`
     background: ${theme.colors.whiteDenim};
     border: none;
     color: ${theme.colors.primary};
+
     i:before {
       color: ${theme.colors.primary};
     }
+
     &:hover {
       color: ${theme.colors.green900};
+
       i:before {
         color: ${theme.colors.green900};
       }
     }
+
     &:focus {
       background-color: ${makeOpaque(theme.colors.green600, 0.1)};
     }
+
     &:disabled {
       background-color: ${theme.colors.whiteDenim};
     }
