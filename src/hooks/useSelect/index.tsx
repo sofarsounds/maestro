fa3265c65feafc517b24c86b5668a8395c349409
jsteinb <jason.steinbach@sofarsounds.com>
2020-12-17 10:40:30 -0500

    cleaner logic in Select effect

--- src/hooks/useSelect/index.tsx
+++ src/hooks/useSelect/index.tsx
@@ -53,11 +53,10 @@ const useSelect = <T extends {}>({
   useEffect(() => {
     if (value) {
       setSelected(value);
-    } else if (value === null && selected !== null) {
+    } else if (value === null && value !== selected) {
       onOptionClick(null);
     }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [value]);
+  }, [value, onOptionClick]);
 
   const toggleSelect = (toOpen: boolean) => {
     if (!toOpen && inputValue) {
